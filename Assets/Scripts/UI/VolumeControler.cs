76c563226e27ac1f8fbe78738fbb04f1af1b335d
Alex <alexandre@guillout.fr>
2016-09-12 03:50:43 +0900

    Volume Update

--- Assets/Scripts/UI/VolumeControler.cs
+++ Assets/Scripts/UI/VolumeControler.cs
@@ -1,15 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class VolumeControler : MonoBehaviour {
 
-   // [SerializedField]
+    [SerializeField]
     float volume;
+    AudioListener activeCam;
 
     void Start()
     {
-        //PlayerPrefs
+        if (PlayerPrefs.GetFloat("Volume") != null)
+        {
+            volume = PlayerPrefs.GetFloat("Volume");
+        }
+        else
+        {
+            volume = 1;
+            PlayerPrefs.SetFloat("Volume", volume);
+        }
+        
     }
 
+    void Update()
+    {
+        if (GameObject.FindObjectOfType<AudioListener>() == true && !activeCam)
+        {
+            activeCam = GameObject.FindObjectOfType<AudioListener>();
+        }
+        else if (activeCam)
+        {
+            //activeCam.volume = volume;
+        }
+    }
 
+    public void SetVolume(float newVolume)
+    {
+        newVolume = GetComponent<Slider>().value;
+        volume = newVolume;
+        PlayerPrefs.SetFloat("Volume", newVolume);
+    }
 }
