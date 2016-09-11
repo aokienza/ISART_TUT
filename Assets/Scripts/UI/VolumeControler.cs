f4f6ec165c607ccf19df564f4a461e5a44139b5c
Alex <alexandre@guillout.fr>
2016-09-12 01:50:30 +0900

    VolumeManager Created

---
+++ Assets/Scripts/UI/VolumeControler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolumeControler : MonoBehaviour {
+
+   // [SerializedField]
+    float volume;
+
+    void Start()
+    {
+        //PlayerPrefs
+    }
+
+
+}
