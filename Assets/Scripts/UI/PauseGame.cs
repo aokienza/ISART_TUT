df4b62a88f07aa8637ed9f124525a072a054646e
Alex <alexandre@guillout.fr>
2016-09-12 00:20:08 +0900

    Pause feature

---
+++ Assets/Scripts/UI/PauseGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGame : MonoBehaviour {
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+    
+    public void UnPause()
+    {
+        Time.timeScale = 1;
+    }
+}
