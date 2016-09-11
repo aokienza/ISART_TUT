9d2940a1b331b9d59cea414f6acaab10b4c5086d
Alex <alexandre@guillout.fr>
2016-09-12 00:03:39 +0900

    ,

---
+++ Assets/Scripts/UI/ScoreDebug.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreDebug : MonoBehaviour {
+
+    [SerializeField]
+    ScoreRecap scoreManager;
+    [SerializeField]
+    Text scoreChoose;
+
+    public void ScoreSubmitDebug()
+    {
+        
+        int scoreDebug1;
+        scoreDebug1 = int.Parse(scoreChoose.text);
+        Debug.Log(scoreChoose.text);
+        scoreManager.SubmitScore(scoreDebug1);
+    }
+}
---
+++ Assets/Scripts/UI/ScoreRecap.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreRecap : MonoBehaviour {
+
+    public int highScore; // High Score saved in game memory
+    //public int score; // Local high score saved if higher than high score
+
+    [SerializeField]
+    Text menuScoreEnd;
+
+    [SerializeField]
+    int[] bestScores;
+
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    // Use this for initialization
+    void Start()
+    {
+        highScore = PlayerPrefs.GetInt("High Score");
+        UpdateMenuScore(highScore);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SubmitScore(int score)
+    {
+        if (score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("High Score", highScore);
+            UpdateMenuScore(highScore);
+        }
+    }
+    
+    void UpdateMenuScore(int score)
+    {
+        Debug.Log(score);
+        menuScoreEnd.text = score.ToString();
+    }
+}
