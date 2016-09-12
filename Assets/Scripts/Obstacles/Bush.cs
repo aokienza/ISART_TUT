a444b3bb2471f356ab44840af8032217a49d2480
enza <m011346763@edu.teu.ac.jp>
2016-09-12 10:19:47 +0900

    Obstacles Object

---
+++ Assets/Scripts/Obstacles/Bush.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bush : MonoBehaviour {
+    public float loss;
+    float velocity;
+	// Use this for initialization
+	void Start () {
+        velocity = GameObject.Find("Player").GetComponent<PlayerController>().velocity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.CompareTag("Player"))
+        {
+            float _velocity = col.gameObject.GetComponent<PlayerController>().velocity;
+            _velocity = _velocity / loss;
+            col.gameObject.GetComponent<PlayerController>().velocity = _velocity;
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.transform.CompareTag("Player"))
+        {
+            col.gameObject.GetComponent<PlayerController>().velocity = velocity;
+        }
+    }
+}
---
+++ Assets/Scripts/Obstacles/Stone.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stone : MonoBehaviour {
+    public float StanTime = 1.0f;
+    float velocity;
+	// Use this for initialization
+	void Start () {
+        velocity = GameObject.Find("Player").GetComponent<PlayerController>().velocity;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.CompareTag("Player"))
+        {
+            Debug.Log("Stone");
+            col.gameObject.GetComponent<PlayerController>().velocity = 0;
+            Invoke("setFloat", StanTime);
+        }
+    }
+
+    void setFloat()
+    {
+        GameObject.Find("Player").GetComponent<PlayerController>().velocity = velocity;
+    }
+}
