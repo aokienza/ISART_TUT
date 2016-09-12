e8d1d6a5be32c87036ed02c076f9242b29869747
enza <m011346763@edu.teu.ac.jp>
2016-09-13 02:00:42 +0900

    Fence

---
+++ Assets/Scripts/Obstacles/Fence_Obstacle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fence_Obstacle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.CompareTag("Player"))
+        {
+            gameObject.GetComponent<BoxCollider>().isTrigger = true;
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.transform.CompareTag("Player"))
+        {
+            gameObject.GetComponent<BoxCollider>().isTrigger = false;
+        }
+    }
+}
