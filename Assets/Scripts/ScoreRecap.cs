174835ba1c7c2d9ecabde78bfb933b8172ddd65e
Alex <alexandre@guillout.fr>
2016-09-13 11:51:52 +0900

    Highscores rework

--- Assets/Scripts/ScoreRecap.cs
+++ Assets/Scripts/ScoreRecap.cs
@@ -13,18 +13,27 @@ public class ScoreRecap : MonoBehaviour
 
     #region UI SCORES
     [SerializeField]
-    Text[] highScoresTexts = new Text[15];
+    Text ScoreLV1;
+    [SerializeField]
+    Text ScoreLV2;
+    [SerializeField]
+    Text ScoreLV3;
+    public int HighScore1;
+    public int HighScore2;
+    public int HighScore3;
+
     #endregion
     bool hasFinishedFont;
     [SerializeField]
     Text menuScoreEnd;
+    [SerializeField]
+    Text menuScoreEndHigh;
+
 
     public LevelManager Manager;
 
     string[,] PlayerName = new string[3,5];
 
-    [SerializeField] // Future feature, sauvegarde de score multiple
-    Dictionary<string, int[]> bestScore = new Dictionary<string, int[]>();
 
 
 
@@ -40,126 +49,50 @@ public class ScoreRecap : MonoBehaviour
     void Start()
     {
         RetrieveBestScore();
-        //UpdateMenuScore(highScore);
     }
     public void RetrieveBestScore()
     {
-        // Initialisation High Score Level 1
-        string[] levels = { "level01", "level02", "level03" };
-        for (int j = 0; j < levels.Length; j++)
-        {
-            if (!bestScore.ContainsKey(levels[j]))
-                bestScore.Add(levels[j], new int[5]);
-            for (int i = 0; i < 5; i++)
-            {
-                bestScore[levels[j]][i] = PlayerPrefs.GetInt(("bestScores" + levels[j]), 0);
-            }
-
-        }
+        HighScore1 = PlayerPrefs.GetInt("HighScore1");
+        HighScore2 = PlayerPrefs.GetInt("HighScore2");
+        HighScore3 = PlayerPrefs.GetInt("HighScore3");
     }
  
     public void SubmitScore(string levelName, int score)
     {
-        if (!bestScore.ContainsKey(levelName))
+        
+        switch(levelName)
         {
-            bestScore.Add(levelName, new int[5]);
-        }
-        for (var i = bestScore[levelName].Length - 1; i >= 0; i--)
-        {
-            if (bestScore[levelName][i] < score)
-            {
-                menuScoreEnd.text = ("YOUR SCORE IS : " + score);
-                InsertScoreInBestScores(bestScore[levelName], score, i);
-                UpdateHighScoresMenu(levelName, score, i);
+            case ("Level01"):
+                if (score >HighScore1)
+                {
+                    HighScore1 = score;
+                    PlayerPrefs.SetInt("HighScore1", HighScore1);
+                    ScoreLV1.text = HighScore1.ToString();
+                }           
                 break;
-            }
-        }
-
-        for (int i = 0; i < bestScore[levelName].Length; i++)
-        {
-            PlayerPrefs.SetInt(("bestScores" + levelName), bestScore[levelName][i]);
-        }
+            case ("Level02"):
+                if (score > HighScore2)
+                {
+                    HighScore2 = score;
+                    PlayerPrefs.SetInt("HighScore2", HighScore2);
+                    ScoreLV2.text = HighScore2.ToString();
+                }
+                break;
+            case ("Level03"):
+                if (score > HighScore3)
+                {
+                    HighScore3 = score;
+                    PlayerPrefs.SetInt("HighScore3", HighScore3);
+                    ScoreLV3.text = HighScore3.ToString();
+                }
+                break;
+        }      
         PlayerPrefs.Save();
     }
     
  
-    void InsertScoreInBestScores(int[] tab, int bestScore, int index)
-    {
-        for (var i = 0; i < index; i++)
-        {
-            tab[i] = tab[i + 1];
-        }
-        tab[index] = bestScore;
-    }
+     #region Update UI Scores
 
-    #region Update UI Scores
-    void UpdateHighScoresMenu(string tableau, int score, int index)
-    {
-        switch (tableau)
-        {
-            case "Level01":
-                switch (index)
-                {
-                    case 0:
-                        highScoresTexts[0].text = ("1) " + score).ToString();
-                        break;
-                    case 1:
-                        highScoresTexts[1].text = ("2) " + score).ToString();
-                        break;
-                    case 2:
-                        highScoresTexts[2].text = ("3) " + score).ToString();
-                        break;
-                    case 3:
-                        highScoresTexts[3].text = ("4) " + score).ToString();
-                        break;
-                    case 4:
-                        highScoresTexts[4].text = ("5) " + score).ToString();
-                        break;
-                }
-                break;
-            case "Level02":
-                switch (index)
-                {
-                    case 0:
-                        highScoresTexts[5].text = ("1) " + score).ToString();
-                        break;
-                    case 1:
-                        highScoresTexts[6].text = ("2) " + score).ToString();
-                        break;
-                    case 2:
-                        highScoresTexts[7].text = ("3) " + score).ToString();
-                        break;
-                    case 3:
-                        highScoresTexts[8].text = ("4) " + score).ToString();
-                        break;
-                    case 4:
-                        highScoresTexts[9].text = ("5) " + score).ToString();
-                        break;
-                }
-                break;
-            case "Level03":
-                switch (index)
-                {
-                    case 0:
-                        highScoresTexts[10].text = ("1) " + score).ToString();
-                        break;
-                    case 1:
-                        highScoresTexts[11].text = ("2) " + score).ToString();
-                        break;
-                    case 2:
-                        highScoresTexts[12].text = ("3) " + score).ToString();
-                        break;
-                    case 3:
-                        highScoresTexts[13].text = ("4) " + score).ToString();
-                        break;
-                    case 4:
-                        highScoresTexts[14].text = ("5) " + score).ToString();
-                        break;
-                }
-                break;
-        }
-
-    }
 
     public IEnumerator FontEffect(Text cible, int size)
     {
